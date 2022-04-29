338670343820034a61336e2bf2e5cdcd0a5f3de9
OlegVorontsov <oleg-voroncov@mail.ru>
2022-04-29 13:10:51 +0300

    Добавьте файлы проекта.

---
+++ Project19.5.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void Voice()
+    {
+        cout << "Звук!" << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "Гав!" << endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "Мяу!" << endl;
+    }
+};
+
+class Pig : public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "Хрю!" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    Dog A, B;
+    Cat C, D;
+    Pig X;
+
+    int const size = 5;
+
+    Animal* Farm[size]{ &A, &C, &X, &D, &B };
+
+    for (int i = 0; i < size; i++)
+    {
+        Farm[i]->Voice();
+    }
+
+    return 0;
+}
